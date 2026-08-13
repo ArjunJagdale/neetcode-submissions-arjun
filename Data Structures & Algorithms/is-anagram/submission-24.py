aa95ee73061b8878863cbd47dba0191fba0a6ed6
Arjun Jagdale <142811259+ArjunJagdale@users.noreply.github.com>
2026-08-13 23:03:34 +0530

    Add: is-anagram - submission-24

---
+++ Data Structures & Algorithms/is-anagram/submission-24.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        h = {}
+        for i in s:
+            h[i] = h.get(i, 0) + 1
+        
+        for x in t:
+            if x not in h or h[x] == 0:
+                return False
+            h[x] -= 1
+        
+        return True
